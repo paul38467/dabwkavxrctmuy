5cc7408a567179275f73d4eb10a43ffc7f56b980
tom <ken61ab@gmail.com>
2018-10-08 22:31:58 +0800

    17. 新增 403, 404, 500 導向頁

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/plugin/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+
+    <!-- Styles -->
+    <style>
+        html {
+          font-size: 18px;
+        }
+
+        body {
+          background-image: url("/images/bg.gif");
+          background-color: #fff;
+          padding-top: 3.5rem;
+        }
+    </style>
+
+    <title>{{ config('app.name') }} - Error 500</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+</head>
+<body>
+    <div class="container">
+        @component('components.alert_with')
+            @slot('color', 'danger')
+            @slot('header', 'Error 500')
+
+            {{ $exception->getMessage() }}
+        @endcomponent
+    </div>
+</body>
+</html>
